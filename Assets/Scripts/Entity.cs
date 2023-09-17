770bd28149ee5746d1cddf88b084cb7a16548f2c
Domms <dommterr@gmail.com>
2023-09-17 16:38:50 +0200

    Created patrolling enemy

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -10,7 +10,12 @@ public class Entity : MonoBehaviour {
     [Header("Collision Information")]
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected float groundCheckDistance;
+    [Space]
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
     [SerializeField] protected LayerMask whatIsGround;
+
+    protected bool isWallDetected;
     protected bool isGrounded;
 
     protected int facingDir = 1;
@@ -19,6 +24,10 @@ public class Entity : MonoBehaviour {
     protected virtual void Start() {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
+
+        if (wallCheck == null) {
+            wallCheck = transform;
+        }
     }
 
     protected virtual void Update() {
@@ -33,9 +42,11 @@ public class Entity : MonoBehaviour {
 
     protected virtual void CollisionChecks() {
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance * facingDir, whatIsGround);
     }
 
     protected virtual void OnDrawGizmos() {
-        Gizmos.DrawLine(transform.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance * facingDir, wallCheck.position.y));
     }
 }
