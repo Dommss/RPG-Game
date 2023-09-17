217a8074b33aacf872c55c7e3401dee8cc2c0b64
Domms <dommterr@gmail.com>
2023-09-18 00:17:20 +0200

    Added jumping and animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour {
 
     [Header("Moving")]
     public float moveSpeed = 8f;
-
+    public float jumpForce;
 
     #region Components
     public Animator anim { get; private set; }
@@ -20,6 +20,8 @@ public class Player : MonoBehaviour {
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
 
     #endregion
 
@@ -28,6 +30,8 @@ public class Player : MonoBehaviour {
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
     }
 
     private void Start() {
--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -25,6 +25,8 @@ public class PlayerState {
 
     public virtual void Update() {
         xInput = Input.GetAxisRaw("Horizontal");
+
+        player.anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     public virtual void Exit() {
