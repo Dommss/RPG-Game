3173dd55e2d64799f99719bbafc2086504e8755f
Domms <dommterr@gmail.com>
2023-09-17 09:00:54 +0200

    Preparation of player script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private float xInput;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
