3ce9a65e07672c28968e450afdd37294a024db6e
Domms <dommterr@gmail.com>
2023-09-17 11:12:44 +0200

    Clean up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,37 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 
     private Rigidbody2D rb;
     private Animator anim;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
-    [SerializeField] private bool isMoving;
 
     private float xInput;
 
-    private void Awake()
-    {
+    private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    private void Update()
-    {
+    private void Update() {
 
+        Movement();
+        CheckInput();
+
+        AnimatorControllers();
+    }
+
+    private void CheckInput() {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Jump();
         }
+    }
 
-        isMoving = rb.velocity.x != 0;
+    private void Movement() {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+
+    private void AnimatorControllers() {
+        bool isMoving = rb.velocity.x != 0;
 
         anim.SetBool("isMoving", isMoving);
     }
