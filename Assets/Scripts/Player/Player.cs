4bf6d25206d0d794669a99d8ef859b315a99723b
Domms <dommterr@gmail.com>
2023-09-19 12:23:16 +0200

    Created Entity.cs inheritance

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : Entity
+{
 
     [Header("Attack")]
     public Vector2[] attackMovement;
@@ -19,23 +20,6 @@ public class Player : MonoBehaviour {
     public float dashDuration;
     public float dashDir { get; private set; }
 
-    [Header("Collision")]
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private Transform wallCheck;
-    [SerializeField] private float wallCheckDistance;
-    [SerializeField] private LayerMask whatIsGround;
-
-    public int facingDir { get; private set; } = 1;
-    private bool facingRight = true;
-
-    #region Components
-    public Animator anim { get; private set; }
-    public Rigidbody2D rb { get; private set; }
-
-    #endregion
-
-
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
 
@@ -51,7 +35,9 @@ public class Player : MonoBehaviour {
 
     #endregion
 
-    private void Awake() {
+    protected override void Awake()
+    {
+        base.Awake();
         stateMachine = new PlayerStateMachine();
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
@@ -65,20 +51,21 @@ public class Player : MonoBehaviour {
         primaryAttackState = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
     }
 
-    private void Start() {
-
-        anim = GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-
+    protected override void Start()
+    {
+        base.Start();
         stateMachine.Initialize(idleState);
     }
 
-    private void Update() {
+    protected override void Update()
+    {
+        base.Update();
         stateMachine.currentState.Update();
         CheckForDashInput();
     }
 
-    public IEnumerator BusyFor(float _seconds) {
+    public IEnumerator BusyFor(float _seconds)
+    {
         isBusy = true;
 
         yield return new WaitForSeconds(_seconds);
@@ -88,60 +75,26 @@ public class Player : MonoBehaviour {
 
     public void AnimationTrigger() => stateMachine.currentState.AnimationFinishTrigger();
 
-    public void CheckForDashInput() {
-        if (isWallDetected()) {
+    public void CheckForDashInput()
+    {
+        if (isWallDetected())
+        {
             return;
         }
 
         dashUsageTimer -= Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0) {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0)
+        {
 
             dashUsageTimer = dashCooldown;
             dashDir = Input.GetAxisRaw("Horizontal");
 
-            if (dashDir == 0) {
+            if (dashDir == 0)
+            {
                 dashDir = facingDir;
             }
 
             stateMachine.ChangeState(dashState);
         }
     }
-
-    #region Velocity
-    public void ZeroVelocity() {
-        rb.velocity = new Vector2(0, 0);
-    }
-
-    public void SetVelocity(float _xVelocity, float _yVelocity) {
-        rb.velocity = new Vector2(_xVelocity, _yVelocity);
-        FlipController(_xVelocity);
-    }
-
-    #endregion
-    #region Collision
-    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-    public bool isWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
-
-    private void OnDrawGizmos() {
-        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
-    }
-
-    #endregion
-    #region Flipping
-    public void Flip() {
-        facingDir = facingDir * -1;
-        facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
-    }
-
-    public void FlipController(float _x) {
-        if (_x > 0 && !facingRight) {
-            Flip();
-        } else if (_x < 0 && facingRight) {
-            Flip();
-        }
-    }
-
-    #endregion
 }
