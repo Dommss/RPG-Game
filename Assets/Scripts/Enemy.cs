15ed145090f8afedcafd311c65989eab6a05f6e9
Domms <dommterr@gmail.com>
2023-09-19 14:24:30 +0200

    Implemented enemy idle and move state

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-
-    public Rigidbody2D rb { get; private set; }
-    public Animator anim { get; private set; }
+public class Enemy : Entity
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float idleTime;
 
     public EnemyStateMachine stateMachine { get; private set; }
 
-    private void Awake() {
+    protected override void Awake()
+    {
+        base.Awake();
         stateMachine = new EnemyStateMachine();
     }
 
-    private void Update() {
+    protected override void Update()
+    {
+        base.Update();
         stateMachine.currentState.Update();
     }
 }
--- Assets/Scripts/EnemyState.cs
+++ Assets/Scripts/EnemyState.cs
@@ -2,32 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyState {
+public class EnemyState
+{
 
     protected EnemyStateMachine stateMachine;
-    protected Enemy enemy;
+    protected Enemy enemyBase;
     private string animBoolName;
 
     protected float stateTimer;
     protected bool triggerCalled;
 
-    public EnemyState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName) {
-        this.enemy = _enemy;
+    public EnemyState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName)
+    {
+        this.enemyBase = _enemy;
         this.stateMachine = _stateMachine;
         this.animBoolName = _animBoolName;
     }
 
-    public virtual void Enter() {
+    public virtual void Enter()
+    {
         triggerCalled = false;
 
-        enemy.anim.SetBool(animBoolName, true);
+        enemyBase.anim.SetBool(animBoolName, true);
     }
 
-    public virtual void Exit() {
-        enemy.anim.SetBool(animBoolName, false);
+    public virtual void Exit()
+    {
+        enemyBase.anim.SetBool(animBoolName, false);
     }
 
-    public virtual void Update() {
+    public virtual void Update()
+    {
         stateTimer -= Time.deltaTime;
     }
 }
