ead8b11da22586e9de457a179ca5b17c83632bf7
Domms <dommterr@gmail.com>
2023-09-19 18:15:41 +0200

    BattleState for enemy created

--- Assets/Scripts/EnemyState.cs
+++ Assets/Scripts/EnemyState.cs
@@ -7,14 +7,16 @@ public class EnemyState
 
     protected EnemyStateMachine stateMachine;
     protected Enemy enemyBase;
+    protected Rigidbody2D rb;
+
     private string animBoolName;
 
     protected float stateTimer;
     protected bool triggerCalled;
 
-    public EnemyState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName)
+    public EnemyState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName)
     {
-        this.enemyBase = _enemy;
+        this.enemyBase = _enemyBase;
         this.stateMachine = _stateMachine;
         this.animBoolName = _animBoolName;
     }
@@ -22,6 +24,7 @@ public class EnemyState
     public virtual void Enter()
     {
         triggerCalled = false;
+        rb = enemyBase.rb;
 
         enemyBase.anim.SetBool(animBoolName, true);
     }
