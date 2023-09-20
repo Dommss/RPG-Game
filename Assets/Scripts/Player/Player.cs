46542066f87f6635e7fd3d94ad0299c4aa3ff468
Domms <dommterr@gmail.com>
2023-09-20 11:31:17 +0200

    Little refactoring of the dash skill

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -15,8 +15,6 @@ public class Player : Entity
     public float jumpForce;
 
     [Header("Dash")]
-    [SerializeField] private float dashCooldown;
-    private float dashUsageTimer;
     public float dashSpeed;
     public float dashDuration;
     public float dashDir { get; private set; }
@@ -85,11 +83,8 @@ public class Player : Entity
             return;
         }
 
-        dashUsageTimer -= Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && SkillManager.instance.dash.CanUseSkill())
         {
-
-            dashUsageTimer = dashCooldown;
             dashDir = Input.GetAxisRaw("Horizontal");
 
             if (dashDir == 0)
