aa84358c09eb5d47879e7a27e4e34dc1749c982b
Domms <dommterr@gmail.com>
2023-09-20 11:14:05 +0200

    PlayerManager and SkillManager singletons

--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class SkillManager : MonoBehaviour
 {
-    public static PlayerManager instance;
-    public Player player;
+    public static SkillManager instance;
 
     private void Awake()
     {
