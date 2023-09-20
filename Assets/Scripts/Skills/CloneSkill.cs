301cf603c6dea4c3350137a069f0e5dcdbbaf675
Domms <dommterr@gmail.com>
2023-09-20 14:41:54 +0200

    Finalized dash skill

--- Assets/Scripts/Skills/CloneSkill.cs
+++ Assets/Scripts/Skills/CloneSkill.cs
@@ -7,11 +7,13 @@ public class CloneSkill : Skill
     [Header("Clone information")]
     [SerializeField] private GameObject clonePrefab;
     [SerializeField] private float cloneDuration;
+    [Space]
+    [SerializeField] private bool canAttack;
 
     public void CreateClone(Transform _clonePosition)
     {
         GameObject newClone = Instantiate(clonePrefab);
 
-        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration);
+        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration, canAttack);
     }
 }
