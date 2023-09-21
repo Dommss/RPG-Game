8be9940b994e848ff968be1da8d49ab1532b9978
Domms <dommterr@gmail.com>
2023-09-21 10:42:20 +0200

    Introduced basic sword throwing

---
+++ Assets/SwordSkillController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSkillController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private Collider2D cd;
+    private Player player;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        cd = GetComponent<CircleCollider2D>();
+        player = GetComponent<Player>();
+    }
+
+    public void SetupSword(Vector2 _dir, float _gravityScale)
+    {
+        rb.velocity = _dir;
+        rb.gravityScale = _gravityScale;
+    }
+}
