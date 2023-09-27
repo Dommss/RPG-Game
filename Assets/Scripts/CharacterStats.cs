abe69d896eb2a0a106b491d6d201071a8f537519
Dominik Pokorný <126150424+Dommss@users.noreply.github.com>
2023-09-27 16:07:17 +0200

    evasion and armor added

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,13 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strength;
-    public Stat damage;
+
+    [Header("Major Stats")]
+    public Stat strength; // 1pt increase dmg by 1 and crit power by 1%
+    public Stat agility; // 1pt increase evasion by 1% and crit chance by 1%
+    public Stat intelligence; // 1pt increase magic damage by 1 and magic resistance by 3
+    public Stat vitality; // 1pt increase health by 3 pts
+
+    [Header("Defensive Stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+    [Header("Offensive Stats")]
+    public Stat damage;
 
     [SerializeField] private int currentHealth;
 
@@ -18,8 +26,30 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats)) return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
+
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
         _targetStats.TakeDamage(totalDamage);
+    }
+
+    private static int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+        }
+        return false;
     }
 
     public virtual void TakeDamage(int _damage)
